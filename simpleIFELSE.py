3f50280a5f5547b664aca5475e418bec9b863533
NatalieZawadi <nataliezawadiwesula@gmail.com>
2025-03-29 20:09:43 +0300

    simpleIFELSE

---
+++ simpleIFELSE.py
@@ -0,0 +1,9 @@
+i = int(input("Enter a number: "))
+if(i < 15):
+    print ("i is smaller than 15")
+    print ("i`m an IF block")
+else:
+    print ("i is greater than 15")
+    print ("i`m an ELSE block")
+    
+print ("i`m not an IF or ELSE block")
